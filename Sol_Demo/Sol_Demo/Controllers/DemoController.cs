df5116ef779ef36cfd383ccee2a94893afce123f
KishorNaik <kishor.naik011.net@gmail.com>
2020-01-14 17:36:53 +0530

    code commit

---
+++ Sol_Demo/Sol_Demo/Controllers/DemoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sol_Demo.Controllers
+{
+    public class DemoController : Controller
+    {
+
+        private List<string> ListImages()
+        {
+            var listObj = new List<String>();
+            listObj.Add("https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg");
+            listObj.Add("https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500");
+            listObj.Add("https://cdn.pixabay.com/photo/2015/06/19/21/24/the-road-815297__340.jpg");
+
+            return listObj;
+        }
+
+        public IActionResult Index()
+        {
+            return View(this.ListImages());
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/TagHelpers/CarouselTagHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Sol_Demo.TagHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("bootstrap-carousel")]
+    public class CarouselTagHelper : TagHelper
+    {
+        #region Declaration
+        private readonly IHtmlHelper helper = null;
+
+        private const String ItemSourceAttributeName = "item-source";
+
+        #endregion
+
+        #region Constructor
+        public CarouselTagHelper(IHtmlHelper helper)
+        {
+            this.helper = helper;
+        }
+        #endregion
+
+        #region Property
+        [HtmlAttributeName(ItemSourceAttributeName)]
+        public List<String> ItemSource { get; set; }
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+        #endregion 
+
+
+        public async override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            //Contextualize the html helper
+            (helper as IViewContextAware).Contextualize(ViewContext);
+
+            var carouselTemplate = await helper.PartialAsync("~/Views/Shared/_BootstrapCarouselTagHelperPartialView.cshtml", ItemSource);
+
+            output.Content.SetHtmlContent(carouselTemplate);
+
+            //return base.ProcessAsync(context, output);
+        }
+    }
+}
